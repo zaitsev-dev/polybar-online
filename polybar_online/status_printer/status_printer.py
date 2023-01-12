5c2e4f1f2aa1728222fc8e22390c2c32f278c000
Vitaliy Zaitsev <dev.zaitsev@gmail.com>
2023-01-12 08:36:20 +0500

    Update doc-strings
    Fixed a typo in doc-string of flush_print(), many doc-strings have been removed because they are redundant. Also, many doc-strings were reformatted for line length according to PEP style rules.

--- polybar_online/status_printer/status_printer.py
+++ polybar_online/status_printer/status_printer.py
@@ -15,17 +15,18 @@ class StatusPrinter:
         offline_notification: str,
     ) -> None:
         """
-        The __init__ function is called when an instance of the class is created.
+        The function is called when an instance of the class is created.
 
         Args:
             self: Refer to the object itself
-            online_str: The output string that will be displayed when the device is online
-            offline_str: The output string that will be displayed when the device is online
-            online_notification: The text that will be sent to the user when they are online
-            offline_notification: The text that will be sent to the user when they are offline
-
-        Returns:
-            None
+            online_str: The output string that will be displayed when
+                the device is online
+            offline_str: The output string that will be displayed when
+                the device is online
+            online_notification: The text that will be sent to the user
+                when they are online
+            offline_notification: The text that will be sent to the user
+                when they are offline
         """
         self._online_output: str = online_str
         self._offline_output: str = offline_str
@@ -33,43 +34,12 @@ class StatusPrinter:
         self._offline_notification = offline_notification
 
     def _print_status(self, status: bool, is_notify: bool) -> None:
-        """
-        The _print_status function prints the output to stdout and sends a notification.
-
-        Args:
-            self: Access the class attributes
-            status: Determine whether the internet connection is up or not
-            is_notify: Determine if a notification should be sent
-
-        Returns:
-            None
-        """
         flush_print(self._online_output if status else self._offline_output)
         if is_notify:
             notify(self._online_notification if status else self._offline_notification)
 
     def print_online_status(self, is_notify: bool) -> None:
-        """
-        Prints the status of the user's connection to the console
-
-        Args:
-            self: Access the attributes and methods of the class in .gitignore
-            is_notify: Determine whether to notify the user that they are offline
-
-        Returns:
-            None
-        """
         self._print_status(status=True, is_notify=is_notify)
 
     def print_offline_status(self, is_notify: bool) -> None:
-        """
-        Prints the status of the user's connection to the console
-
-        Args:
-            self: Access the class attributes
-            is_notify: Determine whether to notify the user that they are offline
-
-        Returns:
-            None
-        """
         self._print_status(status=False, is_notify=is_notify)
